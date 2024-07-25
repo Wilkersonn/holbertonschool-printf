c79c4efd1ff80a2b6ce0bbed62545c2df04fdf42
Wilkersonn <remyfournieroff@gmail.com>
2024-07-25 15:07:47 +0200

    Modified _printf.c
    -Trying to have a correct code.

--- _printf.c
+++ _printf.c
@@ -2,58 +2,9 @@
 #include <unistd.h>
 #include "main.h"
 
-/**
- * print_char - Prints a character.
- * @c: The character to print.
- *
- * Return: The number of characters printed.
- */
-int print_char(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_string - Prints a string.
- * @s: The string to print.
- *
- * Return: The number of characters printed.
- */
-int print_string(char *s)
-{
-	int count = 0;
-
-	if (s == NULL)
-		s = "(null)";
-
-	while (*s)
-	{
-		count += write(1, s, 1);
-		s++;
-	}
-
-	return (count);
-}
-
-/**
- * handle_specifier - Handles the conversion specifiers.
- * @spec: The conversion specifier.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int handle_specifier(char spec, va_list args)
-{
-	if (spec == 'c')
-		return (print_char(va_arg(args, int)));
-	if (spec == 's')
-		return (print_string(va_arg(args, char *)));
-	if (spec == '%')
-		return (write(1, "%", 1));
-
-	write(1, "%", 1);
-	return (write(1, &spec, 1) + 1);
-}
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
 
 /**
  * _printf - Produces output according to a format.
@@ -68,9 +19,6 @@ int _printf(const char *format, ...)
 	int count = 0;
 	const char *p;
 
-	if (format == NULL)
-		return (-1);
-
 	va_start(args, format);
 
 	for (p = format; *p != '\0'; p++)
@@ -78,14 +26,78 @@ int _printf(const char *format, ...)
 		if (*p == '%')
 		{
 			p++;
-			count += handle_specifier(*p, args);
+			switch (*p)
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case '%':
+					count += print_percent();
+					break;
+				default:
+					write(1, "%", 1);
+					write(1, p, 1);
+					count += 2;
+					break;
+			}
 		}
 		else
 		{
-			count += write(1, p, 1);
+			write(1, p, 1);
+			count++;
 		}
 	}
 
 	va_end(args);
 	return (count);
+}
+
+/**
+ * print_char - Prints a character.
+ * @args: The va_list containing the character to print.
+ *
+ * Return: The number of characters printed (always 1).
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: The va_list containing the string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int count = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_percent - Prints a percent sign.
+ *
+ * Return: The number of characters printed (always 1).
+ */
+int print_percent(void)
+{
+	return (write(1, "%", 1));
 }
