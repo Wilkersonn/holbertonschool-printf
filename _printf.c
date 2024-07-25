4bb11cca17d1c6e62ca96db36bdc667c5aa9e53c
Wilkersonn <remyfournieroff@gmail.com>
2024-07-25 15:00:19 +0200

    Modified _printf.c
    remove else if for handle_specifier

--- _printf.c
+++ _printf.c
@@ -2,9 +2,58 @@
 #include <unistd.h>
 #include "main.h"
 
-int print_char(char c);
-int print_string(char *s);
-int handle_specifier(char spec, va_list args);
+/**
+ * print_char - Prints a character.
+ * @c: The character to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Prints a string.
+ * @s: The string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (*s)
+	{
+		count += write(1, s, 1);
+		s++;
+	}
+
+	return (count);
+}
+
+/**
+ * handle_specifier - Handles the conversion specifiers.
+ * @spec: The conversion specifier.
+ * @args: The argument list.
+ *
+ * Return: The number of characters printed.
+ */
+int handle_specifier(char spec, va_list args)
+{
+	if (spec == 'c')
+		return (print_char(va_arg(args, int)));
+	if (spec == 's')
+		return (print_string(va_arg(args, char *)));
+	if (spec == '%')
+		return (write(1, "%", 1));
+
+	write(1, "%", 1);
+	return (write(1, &spec, 1) + 1);
+}
 
 /**
  * _printf - Produces output according to a format.
@@ -19,6 +68,9 @@ int _printf(const char *format, ...)
 	int count = 0;
 	const char *p;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
 
 	for (p = format; *p != '\0'; p++)
@@ -30,83 +82,10 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			write(1, p, 1);
-			count++;
+			count += write(1, p, 1);
 		}
 	}
 
 	va_end(args);
-	return (count);
-}
-
-/**
- * handle_specifier - Handles the conversion specifiers.
- * @spec: The conversion specifier.
- * @args: The argument list.
- *
- * Return: The number of characters printed.
- */
-int handle_specifier(char spec, va_list args)
-{
-	int count = 0;
-	char c;
-	char *s;
-
-	if (spec == 'c')
-	{
-		c = (char)va_arg(args, int);
-		count = print_char(c);
-	}
-	else if (spec == 's')
-	{
-		s = va_arg(args, char *);
-		if (s == NULL)
-			s = "(null)";
-		count = print_string(s);
-	}
-	else if (spec == '%')
-	{
-		write(1, "%", 1);
-		count = 1;
-	}
-	else
-	{
-		write(1, "%", 1);
-		write(1, &spec, 1);
-		count = 2;
-	}
-
-	return (count);
-}
-
-/**
- * print_char - Prints a character.
- * @c: The character to print.
- *
- * Return: The number of characters printed.
- */
-int print_char(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
-
-/**
- * print_string - Prints a string.
- * @s: The string to print.
- *
- * Return: The number of characters printed.
- */
-int print_string(char *s)
-{
-	int count = 0;
-
-	while (*s)
-	{
-		write(1, s, 1);
-		s++;
-		count++;
-	}
-
 	return (count);
 }
