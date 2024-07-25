e9b46963d81b596782f79e3e37f8db5a19c8545c
Wilkersonn <remyfournieroff@gmail.com>
2024-07-25 14:55:08 +0200

    Refactor _printf function to comply with Betty
    -split _printf into smaller function to adhere to the 40 lines limit
    -Created handle_specifier to manage conversation specifiers
    -added print_char and print_string for char and str. printing.
    -Improve readibility

--- _printf.c
+++ _printf.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+int print_char(char c);
+int print_string(char *s);
+int handle_specifier(char spec, va_list args);
 
 /**
  * _printf - Produces output according to a format.
@@ -10,54 +15,98 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *p;
-    char c;
-    char *s;
+	va_list args;
+	int count = 0;
+	const char *p;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (p = format; *p != '\0'; p++)
-    {
-        if (*p == '%')
-        {
-            p++;
-            if (*p == 'c')
-            {
-                c = (char)va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*p == 's')
-            {
-                s = va_arg(args, char *);
-                while (*s)
-                {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-            }
-            else if (*p == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(*p);
-                count += 2;
-            }
-        }
-        else
-        {
-            putchar(*p);
-            count++;
-        }
-    }
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			count += handle_specifier(*p, args);
+		}
+		else
+		{
+			write(1, p, 1);
+			count++;
+		}
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_specifier - Handles the conversion specifiers.
+ * @spec: The conversion specifier.
+ * @args: The argument list.
+ *
+ * Return: The number of characters printed.
+ */
+int handle_specifier(char spec, va_list args)
+{
+	int count = 0;
+	char c;
+	char *s;
+
+	if (spec == 'c')
+	{
+		c = (char)va_arg(args, int);
+		count = print_char(c);
+	}
+	else if (spec == 's')
+	{
+		s = va_arg(args, char *);
+		if (s == NULL)
+			s = "(null)";
+		count = print_string(s);
+	}
+	else if (spec == '%')
+	{
+		write(1, "%", 1);
+		count = 1;
+	}
+	else
+	{
+		write(1, "%", 1);
+		write(1, &spec, 1);
+		count = 2;
+	}
+
+	return (count);
+}
+
+/**
+ * print_char - Prints a character.
+ * @c: The character to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_char(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - Prints a string.
+ * @s: The string to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_string(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+		count++;
+	}
+
+	return (count);
 }
