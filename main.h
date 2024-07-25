22ccad3922ee8621bed678703ecb6de24db8dfb6
Wilkersonn <remyfournieroff@gmail.com>
2024-07-25 17:02:41 +0200

    Modified main.h
    -Added print_int, percent, string, char.
    
    -Added description for function

--- main.h
+++ main.h
@@ -3,9 +3,13 @@
 
 #include <stdarg.h>
 
+/* Function prototype for _printf */
 int _printf(const char *format, ...);
+
+/* Function prototypes for helper functions */
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
